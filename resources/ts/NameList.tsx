982dd43bbd28c7afa20c43fc39c6e4352fbd886f
roy <roy@infolink.com.tw>
2020-07-25 12:48:30 +0800

    update

--- resources/ts/NameList.tsx
+++ resources/ts/NameList.tsx
@@ -3,3 +3,11 @@ export const NameList = {
     sight_coord: "座標方位",
     sight_price: "價格收費"
 }
+
+type ImageList = {
+    img_sample: string
+}
+
+export const ImgList:ImageList = {
+    img_sample: 'https://p2.bahamut.com.tw/S/2KU/17/336a1240a833a8fa514632401f19c6t5.JPG?w=300'
+}
--- resources/ts/components/Sights.tsx
+++ resources/ts/components/Sights.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
-import {NameList} from '../NameList'
+import {NameList, ImgList} from '../NameList'
 import Combobox from 'react-widgets/lib/Combobox'
 
 const Sights : React.FC = () => {
 
-    const [im, setIM] = useState<string>('https://p2.bahamut.com.tw/S/2KU/17/336a1240a833a8fa514632401f19c6t5.JPG?w=300')
+    const [im, setIM] = useState<string>(ImgList.img_sample);
 
     const [options, setOptions] = useState<string[]>(['1','2','3','4','5'])
 
